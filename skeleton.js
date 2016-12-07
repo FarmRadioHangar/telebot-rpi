dd7eb0e5c94046da548a9c11f7427ab52375f580
johanneshilden <hildenjohannes@gmail.com>
2016-12-07 14:26:57 +0300

    Create skeleton.js

---
+++ skeleton.js
@@ -0,0 +1,6 @@
+var TelegramBot     = require('node-telegram-bot-api');
+var JVSDisplayOTron = require('jvsdisplayotron');
+var dothat          = new JVSDisplayOTron.DOTHAT();
+var gpio            = require('rpi-gpio');
+var Omx             = require('node-omxplayer');
+var googleTTS       = require('google-tts-api');
